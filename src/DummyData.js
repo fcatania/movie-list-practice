78c8d473eb6ac987f76ea06156274903fd377768
Felipe Catania <felicatania@gmail.com>
2017-12-25 11:39:44 -0800

    Changed App name and added description to my data.

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,13 +1,12 @@
 var dummyData = [
-  {id: 0, title: 'Star Wars 1'},
-  {id: 1, title: 'Star Wars 2'},
-  {id: 2, title: 'Star Wars 3'},
-  {id: 3, title: 'Star Wars 4'},
-  {id: 4, title: 'Star Wars 5'},
-  {id: 5, title: 'Star Wars 6'},
-  {id: 6, title: 'Star Wars 7'},
-  {id: 7, title: 'Star Wars 8'},
-  {id: 8, title: 'Star Wars 9'},
+  {id: 0, title: 'Star Wars', desc: 'Luke es tu papa.'},
+  {id: 1, title: 'Nacho Libre', desc: 'La mejor pelicula que existe y exisitira.'},
+  {id: 2, title: 'Shrek', desc: 'Somebody once told me the world is gonna roll me.'},
+  {id: 3, title: 'Moana', desc: 'What can I say except you\'re welcome.'},
+  {id: 4, title: 'Zootropolis', desc: 'Robadores de Oscars.'},
+  {id: 5, title: 'Bright', desc: 'Netflix\'s worst movie to date.'},
+  {id: 6, title: 'Mean Girls', desc: 'Wednesdays we wear pink.'},
+  {id: 7, title: 'Thor: Ragnarok', desc: 'Pierde un ojo y el martillo pero gana nuevos poderes.'},
 ];
 
 export default dummyData;
