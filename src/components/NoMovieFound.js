dc7473b491b4b39d823206bb49712f52fb75f98b
Felipe Catania <felicatania@gmail.com>
2017-12-25 11:40:17 -0800

    Added components SearchBar and NoMovieFound.

---
+++ src/components/NoMovieFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NoMovieFound.css';
+
+function NoMovieFound(props) {
+  return <p className="NoMovie-title">I'm telling you gracefully that there are no movies.</p>;
+}
+
+export default NoMovieFound;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <span className="SearchBar">
+        <input className="Search-input" type="text" placeholder="Search by Movie title" onChange={this.props.onInputChange}/>
+        <button className="Search-button" onClick={this.props.searchHandler}>GO</button>
+      </span>
+    );
+  }
+}
+
+
+export default SearchBar;
