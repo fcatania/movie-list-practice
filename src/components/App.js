1bc58ed1e463817c4bbfc24f4836c0f5059bcf38
Felipe Catania <felicatania@gmail.com>
2017-12-25 11:40:29 -0800

    Added search functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieList from './MovieList.js';
+import SearchBar from './SearchBar.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ''
+    }
+  }
+
+  onInputChange(e) {
+    this.setState({searchValue: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Zilio Movies</h1>
         </header>
-        <MovieList />
+        <div className="App-movies-container">
+          <SearchBar onInputChange={this.onInputChange.bind(this)}/>
+          <MovieList searchValue={this.state.searchValue}/>
+        </div>
       </div>
     );
   }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './MovieList.css';
 import movies from '../DummyData.js';
+import NoMovieFound from './NoMovieFound.js';
 
 class MovieList extends Component {
   render() {
     return (
       <div className="MovieList">
         {movies.map((movie) => {
-          return <p key={movie.id}>{movie.title}</p>
+          if (movie.title.includes(this.props.searchValue)) {
+            return <p key={movie.id}>{movie.title}</p>  
+          } else {
+            return null;
+          }
         })}
       </div>
     );
