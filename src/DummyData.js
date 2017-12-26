978c3239fba2227f8728c855ae59b31856a730fb
Felipe Catania <felicatania@gmail.com>
2017-12-25 19:54:16 -0800

    Added watched, all and search filters. All working together.

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,12 +1,12 @@
 var dummyData = [
-  {id: 0, title: 'Star Wars', desc: 'Luke es tu papa.'},
-  {id: 1, title: 'Nacho Libre', desc: 'La mejor pelicula que existe y exisitira.'},
-  {id: 2, title: 'Shrek', desc: 'Somebody once told me the world is gonna roll me.'},
-  {id: 3, title: 'Moana', desc: 'What can I say except you\'re welcome.'},
-  {id: 4, title: 'Zootropolis', desc: 'Robadores de Oscars.'},
-  {id: 5, title: 'Bright', desc: 'Netflix\'s worst movie to date.'},
-  {id: 6, title: 'Mean Girls', desc: 'Wednesdays we wear pink.'},
-  {id: 7, title: 'Thor: Ragnarok', desc: 'Pierde un ojo y el martillo pero gana nuevos poderes.'},
+  {id: 0, title: 'Star Wars', watched: false, desc: 'Luke es tu papa.'},
+  {id: 1, title: 'Nacho Libre', watched: true, desc: 'La mejor pelicula que existe y exisitira.'},
+  {id: 2, title: 'Shrek', watched: true, desc: 'Somebody once told me the world is gonna roll me.'},
+  {id: 3, title: 'Moana', watched: false, desc: 'What can I say except you\'re welcome.'},
+  {id: 4, title: 'Zootropolis', watched: false, desc: 'Robadores de Oscars.'},
+  {id: 5, title: 'Bright', watched: false, desc: 'Netflix\'s worst movie to date.'},
+  {id: 6, title: 'Mean Girls', watched: false, desc: 'Wednesdays we wear pink.'},
+  {id: 7, title: 'Thor: Ragnarok', watched: false, desc: 'Pierde un ojo y el martillo pero gana nuevos poderes.'},
 ];
 
 export default dummyData;
--- src/components/App.js
+++ src/components/App.js
@@ -13,11 +13,16 @@ class App extends Component {
       searchValue: '',
       addValue: '',
       movies: movies,
-      filteredMovies: movies
+      filteredMovies: movies,
+      'all-movies': true,
+      'watched-movies': false
     };
     this.onSearchInputChange = this.onSearchInputChange.bind(this);
     this.onAddInputChange = this.onAddInputChange.bind(this);
     this.addMovie = this.addMovie.bind(this);
+    this.onClickTabAll = this.onClickTabAll.bind(this);
+    this.onClickTabWatched = this.onClickTabWatched.bind(this);
+    this._filterBySearch = this._filterBySearch.bind(this);
   }
 
   addMovie() {
@@ -28,7 +33,7 @@ class App extends Component {
     movieToAdd.desc = 'Some default description for an added movie.';
     currMovies.push(movieToAdd);
     this.setState({movies: currMovies, addValue: ''}, () => {
-      this._filterMovies();
+      this._filterMovies(this._filterBySearch);
     });
   }
 
@@ -36,17 +41,49 @@ class App extends Component {
     this.setState({addValue: e.target.value});
   }
 
+  // As we type in the search bar we apply a the search filter, in addition to that if we are in the watched tab we also filter with the watched filtering function.
   onSearchInputChange(e) {
     this.setState({searchValue: e.target.value}, () => {
-      this._filterMovies();
+      if (this.state['all-movies']) {
+        this._filterMovies(this._filterBySearch);
+      } else {
+        this._filterMovies((movie) => {
+          return this._filterBySearch(movie) && this._filterByWatched(movie);
+        });
+      }
     });
   }
 
-  _filterMovies() {
-    var filteredMovies = this.state.movies.filter((movie) => {
-      return movie.title.includes(this.state.searchValue);
-    });
+  // Helper filtering funcion that returns true if the substring is included in the movie title.
+  _filterBySearch(movie) {
+    return movie.title.includes(this.state.searchValue);
+  }
+
+  // Helper filtering function that returns wheter the movie is watched or not.
+  _filterByWatched(movie) {
+    return movie.watched;
+  }
+
+  // Filters movies with a filtering function as input. Basicly the same as .filter but then modifies the state.
+  _filterMovies(filterFunc) {
+    var filteredMovies = this.state.movies.filter(filterFunc);
     this.setState({filteredMovies: filteredMovies});
+  }
+
+  // Set all-movies true and watched-movies false, then filter by the search value.
+  onClickTabAll(e) {
+    this.setState({'all-movies': true, 'watched-movies': false}, () => {
+      this._filterMovies(this._filterBySearch);
+    });
+  }
+
+  // Set all-movies false and watched-movies true, then filter by the search value and by watched.
+  onClickTabWatched(e) {
+    this.setState({'all-movies': false, 'watched-movies': true}, () => {
+      this._filterMovies((movie) => {
+        return this._filterByWatched(movie) && this._filterBySearch(movie);
+      });
+    });
   }
 
   render() {
@@ -58,8 +95,8 @@ class App extends Component {
         <div className="App-movies-container">
           <AddBar onAddInputChange={this.onAddInputChange} addHandler={this.addMovie} addValue={this.state.addValue}/>
           <div className="App-tab-container">
-            <button className="App-tab-button selected">ALL MOVIES</button>
-            <button className="App-tab-button">WATCHED</button>
+            <button id="all-movies" className={"App-tab-button " + (this.state['all-movies'] ? "selected" : "")} onClick={this.onClickTabAll}>ALL MOVIES</button>
+            <button id="watched-movies" className={"App-tab-button " + (this.state['watched-movies'] ? "selected" : "")} onClick={this.onClickTabWatched}>WATCHED</button>
             <SearchBar onSearchInputChange={this.onSearchInputChange}/>
           </div>
           {this.state.filteredMovies.length > 0 ? <MovieList movies={this.state.filteredMovies} searchValue={this.state.searchValue}/> : <NoMovieFound />}
